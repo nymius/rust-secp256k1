7c06c19abe695e5313a0b8f779a47dbe3707aa1c
nymius <155548262+nymius@users.noreply.github.com>
2025-11-08 19:16:22 -0300

    refactor(silentpayments): redefine PrevoutsSummaryError variants

--- src/silentpayments.rs
+++ src/silentpayments.rs
@@ -25,17 +25,15 @@ impl CPtr for PrevoutsSummary {
     }
 }
 
-/// Label tweak errors
+/// PrevoutsSummary errors.
 #[derive(Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord, Hash)]
 pub enum PrevoutsSummaryError {
-    /// Failed to create the public data
+    /// Failed to create the prevouts summary.
     CreationFailure,
-    /// Serialization Failure
-    SerializationFailure,
-    /// Parse Failure
+    /// Failed to parse the serialized prevout summary.
     ParseFailure,
-    /// Failed to create the shared secret
-    SharedSecretFailure,
+    /// Failed to create the output pubkeys.
+    OutputCreationFailure,
 }
 
 impl PrevoutsSummary {
