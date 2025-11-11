327aa8885e46fe993a848168b43e350d0b98e91b
nymius <155548262+nymius@users.noreply.github.com>
2025-11-11 09:26:24 -0300

    doc(silentpayments): add SAFETY considerations to label_lookup function in examples

--- src/silentpayments/mod.rs
+++ src/silentpayments/mod.rs
@@ -104,10 +104,15 @@ mod test {
         ],
     ];
 
-    #[no_mangle]
-    /// TODO: add docs
+    /// Queries a Rust hash map from C code
+    ///
     /// # Safety
-    /// TODO
+    ///
+    /// The caller must ensure that the cache_ptr is a reference to a valid Rust [`HashMap`], mapping
+    /// from [u8; 33] to [u8; 32] arrays. Any use of other struct is undefined behavior.
+    ///
+    /// The cache_ptr must outlive the returned pointer.
+    #[no_mangle]
     pub unsafe extern "C" fn label_lookup(
         label33: *const c_uchar,
         cache_ptr: *const c_void,
